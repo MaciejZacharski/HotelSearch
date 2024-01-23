dc4850e4942457171e805f8332679f6baa9d682b
Maciej Zacharski <johnzpolski@gmail.com>
2024-01-23 21:46:29 +0100

    Sent checkin and checkout

--- src/main/test/java/HotelSearch.java
+++ src/main/test/java/HotelSearch.java
@@ -22,6 +22,9 @@ public class HotelSearch {
         driver.findElement(By.xpath("//div[@id='select2-drop']//input")).sendKeys("Dubai");
 
         driver.findElement(By.xpath("//span[@class='select2-match' and text()='Dubai']")).click();
+        driver.findElement(By.name("checkin")).sendKeys("21/02/2024");
+        driver.findElement(By.name("checkout")).sendKeys("25/02/2024");
+        
 
 
 
