72f32052223b9ec1691d20cdd4c2441f2e90aba3
Maciej Zacharski <johnzpolski@gmail.com>
2024-01-24 09:10:39 +0100

    Add empty sign up test Add wrong Email format test

--- src/main/test/java/SignUpTest.java
+++ src/main/test/java/SignUpTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import java.util.List;
 import java.util.Map;
@@ -43,6 +44,69 @@ public class SignUpTest {
       Assert.assertTrue(heading.getText().contains(lastName));
       Assert.assertTrue(heading.getText().contains(name));
 
+    }
+    @Test
+    public void EmptySignUp() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("http://www.kurs-selenium.pl/demo/");
+
+        driver.findElements(By.id("li_myaccount")).stream().filter(WebElement::isDisplayed).findFirst().ifPresent(WebElement::click);
+        driver.findElements(By.xpath("//a[text()='  Sign Up']")).get(1).click();
+        driver.findElement(By.xpath("//button[text()=' Sign Up']")).click();
+
+        List<String> errorMessages =    driver.findElements(By.xpath("//div[contains(@class, 'alert')]//p")).stream()
+                .map(el ->el.getAttribute("textContent"))
+                .toList();
+
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(errorMessages.size(), 5);
+        softAssert.assertEquals(errorMessages.get(0),"The Email field is required.");
+        softAssert.assertEquals(errorMessages.get(1), "The Password field is required.");
+        softAssert.assertEquals(errorMessages.get(2),"The Password field is required.");
+        softAssert.assertEquals(errorMessages.get(3),"The First name field is required.");
+        softAssert.assertEquals(errorMessages.get(4),"The Last Name field is required.");
+
+        driver.quit();
+        softAssert.assertAll();
 
     }
+    @Test
+    public void wrongEmailFormat() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("http://www.kurs-selenium.pl/demo/");
+
+        driver.findElements(By.id("li_myaccount")).stream().filter(WebElement::isDisplayed).findFirst().ifPresent(WebElement::click);
+
+        String name = "Test1";
+        String lastName = "Tester1";
+        int random = (int) (Math.random() * 1000);
+        String email = "test" + random + "gmail.com";
+
+        driver.findElements(By.xpath("//a[text()='  Sign Up']")).get(1).click();
+        driver.findElement(By.name("firstname")).sendKeys(name);
+        driver.findElement(By.name("lastname")).sendKeys(lastName);
+        driver.findElement(By.name("phone")).sendKeys("654456654");
+        driver.findElement(By.name("email")).sendKeys(email);
+        driver.findElement(By.name("password")).sendKeys("qwerty");
+        driver.findElement(By.name("confirmpassword")).sendKeys("qwerty");
+        driver.findElement(By.xpath("//button[text()=' Sign Up']")).click();
+
+       String emailFormatMessage = driver.findElement(By.xpath("//div[contains(@class, 'alert')]//p")).getText();
+
+       Assert.assertEquals(emailFormatMessage, "The Email field must contain a valid email address.");
+
+
+
+
+    }
+
+
+
+
 }
